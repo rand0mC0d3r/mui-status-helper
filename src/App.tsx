536fa7580ba373eb9c945c86f4689f316fd05d1c
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-08-14 22:54:40 +0200

    updated lib

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+// import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+// import * as MuiStatus from from 'mui-status'
+// import MuiStatus from 'mui-status'
+import SayHello from 'mui-status'
+import './App.css'
+// import ImplementationFrame from './parts/_ImplementationFrame'
+
+function App() {
+  return (
+    <SayHello name="ddd" />
+  )
+}
+
+export default App
