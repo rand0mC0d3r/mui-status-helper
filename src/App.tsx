a13d7d5af2d90f535e97b4b30f756485e7b994b9
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-10-09 22:08:34 +0200

    updated UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,22 @@ import { MuiStatus, MuiStatusChild, MuiStatusPanel, MuiStatusProvider } from 'mu
 import './App.css'
 
 function App() {
-  return (
-    <div>
-      <MuiStatusProvider position="bottom">
-        <MuiStatus id="dsfdsfsd" >
-          <MuiStatusChild text="ddd" />
-        </MuiStatus>
-        <MuiStatus secondary id="eeewe" >
-          <MuiStatusChild text="eee" />
-        </MuiStatus>
-        <MuiStatusPanel id="fdfdff" popover={<MenuList id="menu-list-grow" onKeyDown={() => { }}>
-          <MenuItem onClick={() => { }}>Profile</MenuItem>
-          <MenuItem onClick={() => { }}>My account</MenuItem>
-          <MenuItem onClick={() => { }}>Logout</MenuItem>
-        </MenuList>}>
-          <MuiStatusChild icon={<ListAltIcon />} text="Menu" />
-        </MuiStatusPanel>
-      </MuiStatusProvider>
-    </div>
-  )
+  return <MuiStatusProvider position="bottom">
+    <MuiStatus id="dsfdsfsd" >
+      <MuiStatusChild text="ddd" />
+    </MuiStatus>
+    <MuiStatus id="eeewe" >
+      <MuiStatusChild text="eee" />
+    </MuiStatus>
+    <MuiStatusPanel id="fdfdff" popover={<MenuList id="menu-list-grow" onKeyDown={() => { }}>
+      <MenuItem onClick={() => { }}>Profile</MenuItem>
+      <MenuItem onClick={() => { }}>My account</MenuItem>
+      <MenuItem onClick={() => { }}>Logout</MenuItem>
+    </MenuList>}>
+      <MuiStatusChild icon={<ListAltIcon />} text="Menu" />
+    </MuiStatusPanel>
+    cxvcx
+  </MuiStatusProvider>
 }
 
 export default App
