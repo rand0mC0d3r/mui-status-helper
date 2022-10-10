170aa238f1f4a92e903561f98df7a58a1a9c8f97
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-10-10 10:48:52 +0200

    updated UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { MenuItem, MenuList } from '@material-ui/core'
+import { MenuItem, MenuList, Tooltip } from '@material-ui/core'
 import ListAltIcon from '@material-ui/icons/ListAlt'
 import { MuiStatus, MuiStatusChild, MuiStatusPanel, MuiStatusProvider } from 'mui-status'
 import './App.css'
 
 function App() {
-  return <MuiStatusProvider position="bottom">
-    <MuiStatus id="dsfdsfsd" >
+  return <MuiStatusProvider position="bottom" tooltipComponent={(tooltip: any, component: any) => <Tooltip arrow title={tooltip}>{component}</Tooltip>}>
+    <MuiStatus id="dsfdsfsd" tooltip={'test'} >
       <MuiStatusChild text="ddd" />
     </MuiStatus>
     <MuiStatus id="eeewe" >
       <MuiStatusChild text="eee" />
+    </MuiStatus>
+    <MuiStatus id="eeewesds" secondary >
+      <MuiStatusChild text="a" />
     </MuiStatus>
     <MuiStatusPanel id="fdfdff" popover={<MenuList id="menu-list-grow" onKeyDown={() => { }}>
       <MenuItem onClick={() => { }}>Profile</MenuItem>
