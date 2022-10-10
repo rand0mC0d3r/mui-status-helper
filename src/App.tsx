b72fe4414479b90877652b8d1dadd0be56882b2d
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-10-10 14:33:35 +0200

    updated UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
+/* eslint-disable no-unused-vars */
 import { MenuItem, MenuList, Tooltip } from '@material-ui/core'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
 import ListAltIcon from '@material-ui/icons/ListAlt'
 import { MuiStatus, MuiStatusChild, MuiStatusPanel, MuiStatusProvider } from 'mui-status'
 import './App.css'
 
 function App() {
-  return <MuiStatusProvider position="bottom" tooltipComponent={(tooltip: any, component: any) => <Tooltip arrow title={tooltip}>{component}</Tooltip>}>
-    <MuiStatus id="dsfdsfsd" tooltip={'test'} >
-      <MuiStatusChild text="ddd" />
+  return <MuiStatusProvider debug position="bottom" tooltipComponent={(tooltip: any, component: any) => <Tooltip arrow title={tooltip}>{component}</Tooltip>}>
+    <MuiStatus {...{ id: 'avatarStatus', style: { minWidth: '48px' }, tooltip: 'Avatar user' }}>
+      <MuiStatusChild mask image='https://picsum.photos/32/32' />
     </MuiStatus>
-    <MuiStatus id="eeewe" >
-      <MuiStatusChild text="eee" />
+    <MuiStatus id="leftWithArrow" tooltip="Sample non interactive Status element">
+      <MuiStatusChild icon={<ArrowBackIosIcon />} text="Test Left" reverse />
     </MuiStatus>
-    <MuiStatus id="eeewesds" secondary >
-      <MuiStatusChild text="a" />
+    <MuiStatus id="leftText" >
+      <MuiStatusChild text="Sample" />
     </MuiStatus>
-    <MuiStatusPanel id="fdfdff" popover={<MenuList id="menu-list-grow" onKeyDown={() => { }}>
+    <MuiStatus id="rightText" secondary tooltip="Sample non interactive Status element">
+      <MuiStatusChild icon={<KeyboardArrowRightIcon />} text="Test Right" />
+    </MuiStatus>
+    <MuiStatus id="rightTextReversed" secondary tooltip="Sample non interactive Status element">
+      <MuiStatusChild icon={<KeyboardArrowRightIcon />} text="Test Right reversed" reverse />
+    </MuiStatus>
+    <MuiStatusPanel id="menu" popover={<MenuList id="menu-list-grow" onKeyDown={() => { }}>
       <MenuItem onClick={() => { }}>Profile</MenuItem>
       <MenuItem onClick={() => { }}>My account</MenuItem>
       <MenuItem onClick={() => { }}>Logout</MenuItem>
